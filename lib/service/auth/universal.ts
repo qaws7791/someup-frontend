6ee01c311b7e8bf76906a0034a18a9928bb93fd1
qaws7791 <qaws7791@gmail.com>
2024-10-02 13:46:32 +0000

    fix: getAccessTokenUniversal 함수에서 액세스 토큰 이름 수정

--- lib/service/auth/universal.ts
+++ lib/service/auth/universal.ts
@@ -1,7 +1,8 @@
+import { accessTokenConfig } from '@/lib/service/auth/constants';
 import { isServerEnvironment } from '@/lib/utils';
 
 export function getAccessTokenUniversal() {
   return isServerEnvironment()
-    ? require('next/headers').cookies().get('access_token').value
+    ? require('next/headers').cookies().get(accessTokenConfig.name).value
     : require('@/lib/service/auth/token').default.accessToken.get();
 }
