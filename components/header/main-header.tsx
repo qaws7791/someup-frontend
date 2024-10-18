39b4166c6bb5eb04de871d36329edb22bce7c648
qaws7791 <qaws7791@gmail.com>
2024-10-18 21:10:18 +0000

    feat: header 컴포넌트 about 페이지 연결

--- components/header/main-header.tsx
+++ components/header/main-header.tsx
@@ -39,7 +39,9 @@ const MainHeader: FunctionComponent = () => {
       <div className="fixed z-50 flex h-17 w-full items-center justify-between bg-white px-6">
         <UserButton user={userQuery.data} />
         <nav className="flex items-center gap-4">
-          <TextButton>소개</TextButton>
+          <TextButton asChild>
+            <Link href="/about">소개</Link>
+          </TextButton>
           <TextButton asChild>
             <Link href="/archive">내 아카이브</Link>
           </TextButton>
@@ -52,7 +54,9 @@ const MainHeader: FunctionComponent = () => {
     return (
       <div className="fixed z-50 flex h-17 w-full items-center justify-end bg-white px-6">
         <nav className="flex items-center gap-4">
-          <TextButton>소개</TextButton>
+          <TextButton asChild>
+            <Link href="/about">소개</Link>
+          </TextButton>
           <Dialog>
             <DialogTrigger asChild>
               <TextButton>로그인</TextButton>
